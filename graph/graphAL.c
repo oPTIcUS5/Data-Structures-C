e38701889a27ceefdd01ce49866d130558f59b85
oPTIcUS5 <axelverastegui1@gmail.com>
2026-07-13 14:39:42 -0600

    Function createGraph ADDED

--- graph/graphAL.c
+++ graph/graphAL.c
@@ -12,3 +12,27 @@ typedef struct Graph
     int vertices;
     Node **list;
 }Graph;
+
+/* Create Functions */
+Graph *createGraph(int numVertices);
+Node *createNode(int destiny);
+
+/* Insert function */
+
+/* Display function*/
+
+/* Delete functions */
+
+Graph *createGraph(int numVertices) {
+
+    Graph *graph = (Graph*)malloc(sizeof(Graph));
+
+    graph->vertices = numVertices;
+
+    graph->list = (Node**)malloc(numVertices * sizeof(Node*));
+
+    for (int i = 0; i < graph->vertices; i++)
+        graph->list[i] = NULL;
+
+    return graph;
+}
